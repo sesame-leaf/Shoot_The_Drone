639b41bb2647c72e609015441db45a53ef9207e8
sesame-leaf <bangjunseok2005@naver.com>
2025-06-18 20:30:20 +0900

    chore: Add UI surface design

--- src/UI_Surface_design.py
+++ src/UI_Surface_design.py
@@ -17,3 +17,6 @@ to_lobby_button_surface = pygame.Surface((80, 50))
 to_lobby_button_surface.fill("#ff0000")
 __to_lobby_button_text = pygame.font.SysFont(None, 20).render("To Lobby", True, "#000000")
 to_lobby_button_surface.blit(__to_lobby_button_text, ((to_lobby_button_surface.get_width()-__to_lobby_button_text.get_width())//2, (to_lobby_button_surface.get_height()-__to_lobby_button_text.get_height())//2))
+
+
+# game_over screen
